ce915520ab3494f5c72d359d2dda9447dcec6b6f
Michael Krasnitski <michael.krasnitski@gmail.com>
2021-01-26 15:22:51 -0500

    fixed MBC1 implementation

--- src/bus.rs
+++ src/bus.rs
@@ -23,13 +23,19 @@ pub struct Mapper {
 
 impl Mapper {
     fn write_byte(&mut self, addr: u16, val: u8) {
+        let rom_mask =
+            (1 << std::cmp::min(5, ((self.num_rom_banks - 1) as f32).log2() as u8 + 1)) - 1;
+        let ram_mask = if self.ram_size <= 8192 {
+            0
+        } else {
+            (1 << (((self.ram_size - 1) as f32 / 8192.0).log2() as u8 + 1)) - 1
+        };
         match self.mapper_type {
             MapperType::ROM => (),
             MapperType::MBC1 => match addr {
                 0x0000..=0x1FFF => self.ram_enabled = (val & 0xf) == 0xA,
                 0x2000..=0x3FFF => {
-                    let mask_size = std::cmp::min(5, 16 - (self.num_rom_banks - 1).leading_zeros());
-                    let mut bank_num = val % (1 << mask_size);
+                    let mut bank_num = val & rom_mask;
                     if bank_num == 0 {
                         bank_num = 1;
                     }
@@ -37,15 +43,36 @@ impl Mapper {
                 }
                 0x4000..=0x5FFF => {
                     let val = val & 0b11;
+                    self.high_bank = ((val << 5) | (self.high_bank & 0x1f)) & rom_mask;
                     if self.flag {
-                        self.ram_bank = val;
-                    } else {
-                        self.high_bank = (self.high_bank & 0x1f) | (val << 5);
+                        self.ram_bank = val & ram_mask;
+                        self.low_bank = (val << 5) & rom_mask;
                     }
                 }
                 0x6000..=0x7FFF => self.flag = val != 0,
+
                 _ => (),
             },
+        }
+    }
+
+    fn get_high_bank(&self) -> u8 {
+        self.high_bank
+    }
+
+    fn get_low_bank(&self) -> u8 {
+        if self.flag {
+            self.low_bank
+        } else {
+            0
+        }
+    }
+
+    fn get_ram_bank(&self) -> u8 {
+        if self.flag {
+            self.ram_bank
+        } else {
+            0
         }
     }
 }
@@ -97,16 +124,16 @@ impl Cartridge {
 
     pub fn read_rom_byte(&self, addr: u16) -> u8 {
         if addr < 0x4000 {
-            self.contents[self.mapper.low_bank as usize * 0x4000 + addr as usize]
+            self.contents[self.mapper.get_low_bank() as usize * 0x4000 + addr as usize]
         } else {
-            self.contents[self.mapper.high_bank as usize * 0x4000 + addr as usize - 0x4000]
+            self.contents[self.mapper.get_high_bank() as usize * 0x4000 + addr as usize - 0x4000]
         }
     }
 
     pub fn read_ram_byte(&self, addr: u16) -> u8 {
         let addr = addr as usize - 0xA000;
         if self.mapper.ram_enabled && addr < self.mapper.ram_size as usize {
-            self.ram[self.mapper.ram_bank as usize * 0x2000 + addr as usize - 0xA000]
+            self.ram[self.mapper.get_ram_bank() as usize * 0x2000 + addr as usize - 0xA000]
         } else {
             0xFF
         }
