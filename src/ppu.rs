4a3fcf3019848823e45cd9dbbd8503a097a1ff67
Michael Krasnitski <michael.krasnitski@gmail.com>
2021-06-14 13:07:20 -0400

    miscellaneous fixes and cleanup
     - OAM is more accurate, now checks 2 sprites per m-cycle instead of all at once
     - Emulate WY = LY edge case (via a latch that sets when the condition is met)
     - Correctly set STAT when writing to it
     - Add memory lock back in

--- src/ppu.rs
+++ src/ppu.rs
@@ -6,6 +6,10 @@ use std::collections::VecDeque;
 use crate::config::Config;
 use crate::display::*;
 
+fn is_bit_set(val: u8, bit: u8) -> bool {
+    val & (1 << bit) != 0
+}
+
 #[derive(Default)]
 struct PPUInterrupts {
     vblank: bool,
@@ -140,7 +144,8 @@ pub struct PPU {
     registers: PPURegisters,
     interrupts: PPUInterrupts,
     viewport: [[Color; W_WIDTH]; W_HEIGHT],
-    sprite_indices: Vec<usize>,
+    oam_sprites: Vec<usize>,
+    oam_index: usize,
     display: Display<W_WIDTH, W_HEIGHT>,
     tile_display: Option<Display<128, 192>>,
 
@@ -152,6 +157,7 @@ pub struct PPU {
     stat_condition: bool,
     ly_coincidence: bool,
     drawing_window: bool,
+    wy_ly_latch: bool,
     dots: u64,
     cycles: u64,
 }
@@ -173,7 +179,8 @@ impl PPU {
             registers: PPURegisters::new(),
             interrupts: PPUInterrupts::default(),
             viewport: [[Color::WHITE; W_WIDTH]; W_HEIGHT],
-            sprite_indices: Vec::new(),
+            oam_sprites: Vec::new(),
+            oam_index: 0,
             display,
             tile_display,
 
@@ -185,6 +192,7 @@ impl PPU {
             stat_condition: false,
             ly_coincidence: false,
             drawing_window: false,
+            wy_ly_latch: false,
             dots: 0,
             cycles: 0,
         })
@@ -193,7 +201,7 @@ impl PPU {
     #[allow(dead_code)]
     fn memory_lock(&self, addr: u16) -> bool {
         let mode = self.get_mode();
-        if self.registers.LCDC & (1 << 7) == 0 {
+        if !is_bit_set(self.registers.LCDC, 7) {
             false
         } else if (0x8000..=0x9FFF).contains(&addr) {
             mode == 3
@@ -205,6 +213,14 @@ impl PPU {
     }
 
     pub fn read_byte(&self, addr: u16) -> u8 {
+        if self.memory_lock(addr) {
+            0xFF
+        } else {
+            self.read_byte_direct(addr)
+        }
+    }
+
+    fn read_byte_direct(&self, addr: u16) -> u8 {
         match addr {
             0x8000..=0x9FFF => self.memory[addr as usize - 0x8000],
             0xFE00..=0xFE9F => self.oam[addr as usize - 0xFE00],
@@ -224,27 +240,32 @@ impl PPU {
     }
 
     pub fn write_byte(&mut self, addr: u16, val: u8) {
-        match addr {
-            0x8000..=0x9FFF => self.memory[addr as usize - 0x8000] = val,
-            0xFE00..=0xFE9F => self.oam[addr as usize - 0xFE00] = val,
-            0xFF40 => {
-                self.registers.LCDC = val;
-                if val & (1 << 7) == 0 {
-                    self.dots = 0;
-                    self.registers.LY = 0;
-                    self.set_mode(0);
+        if !self.memory_lock(addr) {
+            match addr {
+                0x8000..=0x9FFF => self.memory[addr as usize - 0x8000] = val,
+                0xFE00..=0xFE9F => self.oam[addr as usize - 0xFE00] = val,
+                0xFF40 => {
+                    self.registers.LCDC = val;
+                    if !is_bit_set(self.registers.LCDC, 7) {
+                        self.dots = 0;
+                        self.registers.LY = 0;
+                        self.set_mode(0);
+                    }
                 }
+                0xFF41 => {
+                    self.registers.STAT &= 0b10000111; // these bits are read-only
+                    self.registers.STAT |= val & 0b01111000;
+                }
+                0xFF42 => self.registers.SCY = val,
+                0xFF43 => self.registers.SCX = val,
+                0xFF45 => self.registers.LYC = val,
+                0xFF47 => self.registers.BGP = val,
+                0xFF48 => self.registers.OBP0 = val,
+                0xFF49 => self.registers.OBP1 = val,
+                0xFF4A => self.registers.WY = val,
+                0xFF4B => self.registers.WX = val,
+                _ => panic!("Invalid PPU Register write: {:04x}", addr),
             }
-            0xFF41 => self.registers.STAT |= val & !7,
-            0xFF42 => self.registers.SCY = val,
-            0xFF43 => self.registers.SCX = val,
-            0xFF45 => self.registers.LYC = val,
-            0xFF47 => self.registers.BGP = val,
-            0xFF48 => self.registers.OBP0 = val,
-            0xFF49 => self.registers.OBP1 = val,
-            0xFF4A => self.registers.WY = val,
-            0xFF4B => self.registers.WX = val,
-            _ => panic!("Invalid PPU Register write: {:04x}", addr),
         }
     }
 
@@ -287,7 +308,7 @@ impl PPU {
         // Keep drawing a blank screen while the LCD is turned off
         // This means occasionally decoupling the display "cycles"
         // from the number of LCD dots that have passed.
-        if self.registers.LCDC & (1 << 7) != 0 {
+        if is_bit_set(self.registers.LCDC, 7) {
             self.step();
             self.cycles = self.dots + 1;
             self.dots = (self.dots + 1) % 17556;
@@ -308,13 +329,20 @@ impl PPU {
             self.registers.LY = scanline;
         }
         self.check_LY_coincidence();
+        if self.registers.WY == self.registers.LY {
+            self.wy_ly_latch = true;
+        }
 
         // PPU Mode switching
         if self.registers.LY < 144 {
-            if clocks == 0 {
-                self.set_mode(2);
+            if clocks < 20 {
+                if clocks == 0 {
+                    self.set_mode(2);
+                    self.oam_sprites.clear();
+                    self.oam_index = 0;
+                }
                 self.oam_scan();
-            } else if clocks >= 20 && self.get_mode() != 0 {
+            } else if self.get_mode() != 0 {
                 if clocks == 20 {
                     self.set_mode(3);
                     self.draw_line();
@@ -325,14 +353,17 @@ impl PPU {
         } else {
             if self.registers.LY == 144 && clocks == 0 {
                 self.interrupts.vblank = true;
+                if is_bit_set(self.registers.STAT, 5) {
+                    self.interrupts.stat = true;
+                }
             }
+            self.wy_ly_latch = false;
             self.set_mode(1);
         }
     }
 
     fn set_mode(&mut self, mode: u8) {
-        self.registers.STAT &= !0b11;
-        self.registers.STAT |= mode & 0b11;
+        self.registers.STAT = (self.registers.STAT & !0b11) | (mode & 0b11);
         if mode < 3 {
             self.update_stat_condition();
         }
@@ -355,7 +386,7 @@ impl PPU {
 
     fn update_stat_condition(&mut self) {
         let old_val = self.stat_condition;
-        self.stat_condition = (self.registers.STAT & (1 << 6) != 0) && self.ly_coincidence;
+        self.stat_condition = is_bit_set(self.registers.STAT, 6) && self.ly_coincidence;
         for i in 0..3 {
             if self.registers.STAT & (1 << (i + 3)) != 0 {
                 self.stat_condition |= self.get_mode() == i;
@@ -367,25 +398,26 @@ impl PPU {
     }
 
     fn oam_scan(&mut self) {
-        let sprite_height = ((self.registers.LCDC & (1 << 2)) >> 2) as u8 + 1;
-        self.sprite_indices.clear();
-        for i in 0..40 {
-            if self.sprite_indices.len() < 10 {
+        let sprite_height = match is_bit_set(self.registers.LCDC, 2) {
+            true => 16,
+            false => 8,
+        };
+        for i in self.oam_index..self.oam_index + 2 {
+            if self.oam_sprites.len() < 10 {
                 let sprite_y = self.oam[4 * i];
                 let sprite_x = self.oam[4 * i + 1];
-                if sprite_x > 0
-                    && self.registers.LY + 16 >= sprite_y
-                    && self.registers.LY + 16 < sprite_y + 8 * sprite_height
-                {
+                let sprite_pos = self.registers.LY + 16 - sprite_y;
+                if sprite_x > 0 && (0..sprite_height).contains(&sprite_pos) {
                     let get_sprite_x = |j| self.oam[4 * j as usize + 1];
                     let idx = self
-                        .sprite_indices
+                        .oam_sprites
                         .binary_search_by(|&j| sprite_x.cmp(&get_sprite_x(j)))
                         .unwrap_or_else(|j| j);
-                    self.sprite_indices.insert(idx, i);
+                    self.oam_sprites.insert(idx, i);
                 }
             }
         }
+        self.oam_index += 2;
     }
 
     fn draw_fifo(&mut self) {
@@ -408,27 +440,28 @@ impl PPU {
                 self.step_fetcher();
             } else if self.bg_fifo.num_pixels < 160 {
                 // Pop pixels until we have 160 on the line, then mode 3 is done
-                if let Some(pixel) = self.bg_fifo.queue.pop_front() {
-                    if self.registers.LCDC & (1 << 7) != 0 {
-                        if self.registers.LCDC & 1 != 0 {
+                if is_bit_set(self.registers.LCDC, 7) {
+                    if is_bit_set(self.registers.LCDC, 0) {
+                        if let Some(pixel) = self.bg_fifo.queue.pop_front() {
                             let x = self.bg_fifo.num_pixels as usize;
                             let y = self.registers.LY as usize;
                             self.fifo_viewport[y][x] = pixel.decode();
                             self.bg_fifo.num_pixels += 1;
                         }
                     }
+                    // Check if we're inside the window, and reset the FIFO if we are
+                    let window_x = self.bg_fifo.num_pixels + 7;
+                    if is_bit_set(self.registers.LCDC, 5)
+                        && (self.registers.WX..167).contains(&window_x)
+                        && self.wy_ly_latch
+                        && !self.drawing_window
+                    {
+                        self.drawing_window = true;
+                        self.bg_fifo.fetcher.reset();
+                        self.bg_fifo.queue.clear();
+                    }
+                    self.step_fetcher();
                 }
-                // Check if we're inside the window, and reset the FIFO if we are
-                if self.registers.LCDC & (1 << 5) != 0
-                    && self.registers.LY >= self.registers.WY
-                    && self.bg_fifo.num_pixels + 7 >= self.registers.WX
-                    && !self.drawing_window
-                {
-                    self.drawing_window = true;
-                    self.bg_fifo.fetcher.reset();
-                    self.bg_fifo.queue.clear();
-                }
-                self.step_fetcher();
             } else {
                 self.set_mode(0);
                 if self.drawing_window {
@@ -449,7 +482,7 @@ impl PPU {
                         true => 6,
                         false => 3,
                     };
-                    let tilemap = match self.registers.LCDC & (1 << tilemap_bit) != 0 {
+                    let tilemap = match is_bit_set(self.registers.LCDC, tilemap_bit) {
                         true => 0x9C00,
                         false => 0x9800,
                     };
@@ -468,7 +501,7 @@ impl PPU {
                     };
                     // Fetch the tile number from the tilemap
                     let tile_addr = tilemap + 32 * (y / 8) as u16 + (x / 8) as u16;
-                    self.bg_fifo.fetcher.tile_num = self.read_byte(tile_addr);
+                    self.bg_fifo.fetcher.tile_num = self.read_byte_direct(tile_addr);
                     self.bg_fifo.fetcher.row_num = y % 8;
                 }
                 FetcherState::GetLoByte => {
@@ -476,16 +509,17 @@ impl PPU {
                     let tile_num = self.bg_fifo.fetcher.tile_num;
                     let row_num = self.bg_fifo.fetcher.row_num;
                     self.bg_fifo.fetcher.tile_addr =
-                        match self.registers.LCDC & (1 << 4) == 0 && tile_num < 0x80 {
+                        match !is_bit_set(self.registers.LCDC, 4) && tile_num < 0x80 {
                             true => 0x9000,
                             false => 0x8000,
                         } + 16 * tile_num as u16
                             + 2 * row_num as u16;
-                    self.bg_fifo.fetcher.tile_lo = self.read_byte(self.bg_fifo.fetcher.tile_addr);
+                    self.bg_fifo.fetcher.tile_lo =
+                        self.read_byte_direct(self.bg_fifo.fetcher.tile_addr);
                 }
                 FetcherState::GetHiByte => {
                     self.bg_fifo.fetcher.tile_hi =
-                        self.read_byte(self.bg_fifo.fetcher.tile_addr + 1);
+                        self.read_byte_direct(self.bg_fifo.fetcher.tile_addr + 1);
                 }
                 FetcherState::Sleep => {}
             }
@@ -508,17 +542,17 @@ impl PPU {
     fn draw_line(&mut self) {
         let mut scanline = [Pixel::default(); W_WIDTH];
         // LCD Enable
-        if self.registers.LCDC & (1 << 7) != 0 {
-            if self.registers.LCDC & 1 != 0 {
+        if is_bit_set(self.registers.LCDC, 7) {
+            if is_bit_set(self.registers.LCDC, 0) {
                 // Background
-                let bg_tilemap = match self.registers.LCDC & (1 << 3) != 0 {
+                let bg_tilemap = match is_bit_set(self.registers.LCDC, 3) {
                     true => 0x9C00,
                     false => 0x9800,
                 };
                 let bg_y = self.registers.SCY.wrapping_add(self.registers.LY);
                 for i in 0u8..32 {
                     let bg_tile_num =
-                        self.read_byte(bg_tilemap + 32 * ((bg_y / 8) as u16) + i as u16);
+                        self.read_byte_direct(bg_tilemap + 32 * ((bg_y / 8) as u16) + i as u16);
                     let bg_tile_row = self.decode_tile_row(bg_tile_num, bg_y % 8, false);
                     for j in 0u8..8 {
                         let bg_x = (8 * i + j).wrapping_sub(self.registers.SCX) as usize;
@@ -532,13 +566,13 @@ impl PPU {
                 }
 
                 // Window
-                if self.registers.LCDC & (1 << 5) != 0 && self.registers.LY >= self.registers.WY {
-                    let win_tilemap = match self.registers.LCDC & (1 << 6) != 0 {
+                if is_bit_set(self.registers.LCDC, 5) && self.wy_ly_latch {
+                    let win_tilemap = match is_bit_set(self.registers.LCDC, 6) {
                         true => 0x9C00,
                         false => 0x9800,
                     };
                     for i in 0u8..32 {
-                        let win_tile_num = self.read_byte(
+                        let win_tile_num = self.read_byte_direct(
                             win_tilemap + 32 * ((self.registers.WC / 8) as u16) + i as u16,
                         );
                         let win_tile_row =
@@ -558,27 +592,27 @@ impl PPU {
 
             // Sprites
             let sprite_height = ((self.registers.LCDC & (1 << 2)) >> 2) as u8 + 1;
-            if self.registers.LCDC & (1 << 1) != 0 {
-                for sprite_index in self.sprite_indices.iter() {
+            if is_bit_set(self.registers.LCDC, 1) {
+                for sprite_index in self.oam_sprites.iter() {
                     let sprite_y = self.oam[4 * sprite_index];
                     let sprite_x = self.oam[4 * sprite_index + 1];
                     let sprite_tile_num =
                         self.oam[4 * sprite_index + 2] & (0xFF - sprite_height + 1);
                     let sprite_flags = self.oam[4 * sprite_index + 3];
 
-                    let sprite_palette = if sprite_flags & (1 << 4) != 0 {
+                    let sprite_palette = if is_bit_set(sprite_flags, 4) {
                         self.registers.OBP1
                     } else {
                         self.registers.OBP0
                     };
 
                     let mut row_num = self.registers.LY + 16 - sprite_y;
-                    if sprite_flags & (1 << 6) != 0 {
+                    if is_bit_set(sprite_flags, 6) {
                         row_num = 8 * sprite_height - 1 - row_num
                     };
                     let sprite_tile_row = self.decode_tile_row(sprite_tile_num, row_num, true);
                     for j in 0..8 {
-                        let col_num = if sprite_flags & (1 << 5) != 0 {
+                        let col_num = if is_bit_set(sprite_flags, 5) {
                             7 - j
                         } else {
                             j
@@ -587,7 +621,7 @@ impl PPU {
                         let color_num = sprite_tile_row[col_num];
                         if (0..W_WIDTH).contains(&obj_x)
                             && color_num != 0
-                            && (sprite_flags & (1 << 7) == 0 || scanline[obj_x].color == 0)
+                            && (!is_bit_set(sprite_flags, 7) || scanline[obj_x].color == 0)
                         {
                             scanline[obj_x] = Pixel {
                                 color: color_num,
@@ -611,8 +645,8 @@ impl PPU {
             let tile_y = i / 16;
             let tile_x = i % 16;
             for j in 0..8 {
-                let hi = self.read_byte(tile_addr + 2 * j + 1);
-                let lo = self.read_byte(tile_addr + 2 * j);
+                let hi = self.read_byte_direct(tile_addr + 2 * j + 1);
+                let lo = self.read_byte_direct(tile_addr + 2 * j);
                 for k in 0..8 {
                     let pixel = Pixel {
                         color: (((hi >> k) & 1) << 1) | ((lo >> k) & 1),
@@ -628,14 +662,14 @@ impl PPU {
     fn decode_tile_row(&self, tile_num: u8, row_num: u8, is_sprite: bool) -> [u8; 8] {
         let mut row = [0; 8];
         let tile_row_offset =
-            if self.registers.LCDC & (1 << 4) == 0 && tile_num < 0x80 && !is_sprite {
+            if !is_bit_set(self.registers.LCDC, 4) && tile_num < 0x80 && !is_sprite {
                 0x9000
             } else {
                 0x8000
             } + 16 * tile_num as u16
                 + 2 * row_num as u16;
-        let hi = self.read_byte(tile_row_offset + 1);
-        let lo = self.read_byte(tile_row_offset);
+        let hi = self.read_byte_direct(tile_row_offset + 1);
+        let lo = self.read_byte_direct(tile_row_offset);
         for i in 0..8 {
             row[7 - i] = (((hi >> i) & 1) << 1) | ((lo >> i) & 1);
         }
